590316e301d5b04bc3d236c913a2a577d01c9ae7
Sodiq Alabi <adisco4420@gmail.com>
2018-06-30 16:55:22 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-var appCacheName = 'currency-converter';
+var appCacheName = 'CurrencyXchange';
 var appCacheAssets = [
-	'https://adisco4420.github.io/currency-converter/',
-	'https://adisco4420.github.io/currency-converter/css/main.css',
-	'https://adisco4420.github.io/currency-converter/script/main.js',
-	'https://adisco4420.github.io/currency-converter/img/icon.png',
-	'https://adisco4420.github.io/currency-converter/package.json',
+	'https://adisco4420.github.io/CurrencyXchange/',
+	'https://adisco4420.github.io/CurrencyXchange/css/main.css',
+	'https://adisco4420.github.io/CurrencyXchange/script/main.js',
+	'https://adisco4420.github.io/CurrencyXchange/img/icon.png',
+	'https://adisco4420.github.io/CurrencyXchange/package.json',
 	'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css',
 	'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
 	'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js',
